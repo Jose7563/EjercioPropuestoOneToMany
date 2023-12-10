e1e3617d2b50b8a161dd7aa93ac70549243557ff
Jose7563 <juanes7563@gmail.com>
2023-12-10 10:59:37 -0500

    nuevo

---
+++ JasperTool/src/main/java/com/cibertec/JasperToolApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperToolApplication.class, args);
+	}
+
+}
---
+++ JasperTool/src/main/java/com/cibertec/enity/Nuevo.java
@@ -0,0 +1,5 @@
+package com.cibertec.enity;
+
+public class Nuevo {
+
+}
